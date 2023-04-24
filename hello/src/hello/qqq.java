1a32b031b81faac7f1866ea7082f7ecc31f99f66
musichua <782486741@qq.com>
2023-04-24 21:15:11 +0800

    提交项目代码

---
+++ hello/src/hello/qqq.java
@@ -0,0 +1,19 @@
+package hello;
+
+import java.io.File;
+import java.io.IOException;
+
+public class qqq {
+
+	public static void main(String[] args) throws Exception {
+		File file = new File("D:/a");
+		boolean b=file.exists();
+		System.out.println(b);
+		boolean b1=file.createNewFile();
+		System.out.println(b1);
+		boolean b2=file.delete();
+		System.out.println(b2);
+		
+		
+	}
+}
