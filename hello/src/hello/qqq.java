69964b20b8a4920188e2c049421fe5aed91bc237
musichua <782486741@qq.com>
2023-05-08 20:52:36 +0800

    dev提交

--- hello/src/hello/qqq.java
+++ hello/src/hello/qqq.java
@@ -6,16 +6,7 @@ import java.io.IOException;
 public class qqq {
 
 	public static void main(String[] args) throws Exception {
-		File file = new File("D:/a");
-		boolean b=file.exists();
-		System.out.println(b);
-		boolean b1=file.createNewFile();
-		System.out.println(b1);
-		boolean b2=file.delete();
-		System.out.println(b2);
-		//我改了这里的内容--dev-第二次
-		
-		//我也在这里改了内容，我是dev
+		//我是dev，我也改了这里的文件，要造冲突
 		
 	}
 }
